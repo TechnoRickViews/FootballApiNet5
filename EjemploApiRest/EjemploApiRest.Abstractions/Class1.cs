3bff9785efbddc7073c88d06ed0aa7dffbe53b3f
Ricardo Toapanta <hrtoapanta@gmail.com>
2022-07-24 00:03:30 +0200

    creacion de abstraccion del CRUD y configuracion de Inyeccion de dependencias de las capas de Application y Repository

--- EjemploApiRest/EjemploApiRest.Abstractions/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EjemploApiRest.Abstractions
-{
-    public class Class1
-    {
-    }
-}
---
+++ EjemploApiRest/EjemploApiRest.Application/Application.cs
@@ -0,0 +1,59 @@
+﻿using EjemploApiRest.Abstractions;
+using Repository;
+using System;
+using System.Collections.Generic;
+
+namespace EjemploApiRest.Application
+{
+
+    //Esta capa va a tener las 4 operaciones basicas del CRUD ya que todas las peticiones van a pasar por la capa de Application y Repository
+    //vamos a trabajar con objectos Genericos 
+
+
+    //para poder construir inyeccion de dependencias necesitamos una Interfaz que represente a la aplicacion 
+    // las 4 operaciones las vamos a usar desde una clase de abstraccion ICrud
+
+    public interface IApplication<T>:ICrud<T>
+    {
+
+    }
+
+    public class Application<T> : IApplication<T>
+    {
+
+        //Aplication va a tener un constructor que recibe un parametro 
+        //en este caso el parametro va a ser el repositorio de tipo generico 
+        //Con este constructor y parametro estamos inyectando la dependencia 
+        //En vez de crear la instancia del repositorio dentro de la clase Application 
+        //inyectamos la dependencia ya instanciada en el contenedor de dependencias por tanto repository va  venir instanciado 
+
+        IRepository<T> _repository;
+        public Application (IRepository<T> repository)
+        {
+            //guardamos la instancia del repositorio en un objeto local 
+            _repository = repository;
+        }
+
+        //Adaptamos los metodos del CRUD para pasarlos directamente a la capa Repository ya que Application solo es una capa 
+        //de pasamanos, la capa Application pasa directamente los datos a Repository
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public IList<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public T Save(T entity)
+        {
+            return _repository.Save(entity);
+        }
+    }
+}
